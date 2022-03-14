0b9ed097310ce8012e58e8050aab482c600a836c
Hasibur Rahman <hasib.active@gmail.com>
2022-03-14 16:53:08 +0600

    updated

--- src/components/TheHeader.vue
+++ src/components/TheHeader.vue
@@ -9,21 +9,21 @@
 
       <!-- Menu -->
       <div>
-         <ul class="flex">
+         <ul class="menu-ul flex">
             <li>
-               <router-link to="/" class="q-pa-sm q-mx-sm text-weight-medium text-grey-9">Home</router-link>
+               <router-link to="/" class=" q-pa-sm q-mx-sm text-weight-medium text-grey-9">Home</router-link>
             </li>
             <li>
-               <router-link to="/" class="q-pa-sm q-mx-sm text-weight-medium text-grey-9">About Us</router-link>
+               <router-link to="/about" class=" q-pa-sm q-mx-sm text-weight-medium text-grey-9">About Us</router-link>
             </li>
             <li>
-               <router-link to="/" class="q-pa-sm q-mx-sm text-weight-medium text-grey-9">Contact Us</router-link>
+               <router-link to="/contact" class=" q-pa-sm q-mx-sm text-weight-medium text-grey-9">Contact Us</router-link>
             </li>
             <li>
-               <router-link to="/" class="q-pa-sm q-mx-sm text-weight-medium text-grey-9">Restaurants</router-link>
+               <router-link to="/restaurants" class=" q-pa-sm q-mx-sm text-weight-medium text-grey-9">Restaurants</router-link>
             </li>
             <li>
-               <router-link to="/" class="q-pa-sm q-mx-sm text-weight-medium text-grey-9">Foods</router-link>
+               <router-link to="/foods" class=" q-pa-sm q-mx-sm text-weight-medium text-grey-9">Foods</router-link>
             </li>
          </ul>
       </div>
@@ -43,5 +43,16 @@ export default {
 }
 </script>
 
-<style lang="scss" scoped>
+<style lang="scss">
+.menu-ul {
+   > li > a {
+      &:hover {
+         color: $primary !important;
+      }
+   }
+
+   .router-link-exact-active {
+      color: $primary !important;
+   }
+}
 </style>
--- src/pages/Index.vue
+++ src/pages/Index.vue
@@ -1,49 +1,17 @@
 <template>
    <div>
-      <!-- Hero Area -->
-      <div class="hero-area q-px-xl">
-         <div>
-            <h2 class="title ">
-               <span class="b">Food</span> You Love,
-
-               <br>
-
-               Delivered <span class="b">To You.</span>
-            </h2>
-
-            <div class="q-mt-lg">
-               <img src="https://lh3.googleusercontent.com/cjsqrWQKJQp9RFO7-hJ9AfpKzbUb_Y84vXfjlP0iRHBvladwAfXih984olktDhPnFqyZ0nu9A5jvFwOEQPXzv7hr3ce3QVsLN8kQ2Ao=s0" alt="Apple">
-               <img src="https://developer.apple.com/assets/elements/badges/download-on-the-app-store.svg" alt="Apple" style="height: 45px;">
-            </div>
-         </div>
-      </div>
+      <web-home />
    </div>
 </template>
 
 <script>
+import WebHome from 'components/pages/WebHome.vue'
 export default {
-
+   components: {
+      WebHome,
+   },
 }
 </script>
 
 <style lang="scss" scoped>
-.hero-area {
-   background-image: url("assets/images/hermes-rivera.png");
-   background-repeat: no-repeat;
-   background-size: cover;
-   background-position: center center;
-   background-color: #fff;
-
-   height: 380px;
-   display: flex;
-   align-items: center;
-
-   .title {
-      color: #30314a;
-      line-height: 70px;
-      .b {
-         font-weight: bold;
-      }
-   }
-}
 </style>
