d89fce759deaa4f816f3df07cd89e3724b729361
Hasibur Rahman <hasib.active@gmail.com>
2022-03-23 22:21:18 +0600

    update auth

--- src/components/AuthModal.vue
+++ src/components/AuthModal.vue
@@ -17,12 +17,12 @@
 
                   <!-- Password -->
                   <label class="block">Password</label>
-                  <q-input dense v-model="signinForm.password" type="password" placeholder="Enter your Password" :rules="[val => !!val || 'Password is required']" />
+                  <q-input dense v-model="signinForm.password" type="password" placeholder="Enter your Password" ref="password" :rules="[val => !!val || 'Password is required']" />
 
                   <input type="submit" value="" class="hidden">
                </form>
 
-               <p class="text-right q-mt-md text-teal">Forget Password?</p>
+               <p @click="mode = 'forget'" class="text-right q-mt-md text-teal cursor-pointer">Forget Password?</p>
                <p class="q-mt-md">Don't have any account? <span @click="mode = 'signup'" class="text-teal cursor-pointer">Register Here</span> </p>
             </q-card-section>
 
@@ -72,6 +72,62 @@
                <q-btn flat label="Signup" @click="onSignup" />
             </q-card-actions>
          </q-card>
+
+         <!-- Forget Password -->
+         <q-card v-if="mode === 'forget'" style="min-width: 390px">
+            <q-card-section>
+               <div class="text-h6">Reset password</div>
+            </q-card-section>
+
+            <q-card-section>
+               <form @submit.prevent="sendOtp">
+                  <!-- Email -->
+                  <label class="block">Email</label>
+                  <q-input autofocus dense v-model="forgetForm.email" type="text" placeholder="Enter your Email" ref="email" :rules="[val => !!val || 'Email is required']" />
+
+                  <q-btn no-caps @click="sendOtp" type="submit" color="primary" label="Send Mail" class="q-mt-lg" />
+               </form>
+
+               <p class="q-mt-lg">
+                  Already have an account?
+                  <span @click="mode = 'signin'" class="text-primary cursor-pointer">Please Signin</span>
+                  or
+                  <span @click="mode = 'signup'" class="text-primary cursor-pointer">Create new</span>
+               </p>
+            </q-card-section>
+         </q-card>
+         <!-- Reset Password -->
+         <q-card v-if="mode === 'otp'" style="min-width: 390px">
+            <q-card-section>
+               <div class="text-h6">Reset password</div>
+            </q-card-section>
+
+            <q-card-section>
+               <form @submit.prevent="submitOTP">
+                  <!-- Otp -->
+                  <label class="block">Otp</label>
+                  <q-input dense v-model="resetForm.otp" type="text" placeholder="Enter Otp code" ref="otp" :rules="[val => !!val || 'Otp code is required']" />
+
+                  <!-- Password -->
+                  <label class="block">Password</label>
+                  <q-input dense v-model="resetForm.new_password" type="password" placeholder="Enter your Password" ref="password" :rules="[val => !!val || 'Password is required', val => val.length > 7 || 'Password must be atleast 8 characters long.']" />
+
+                  <!-- Confirmed password -->
+                  <label class="block">Confirmed Password</label>
+                  <q-input dense v-model="resetConfirmed" type="password" placeholder="Confirmed Password" ref="confirmed-password" :rules="[val => !!val || 'Please confirm your Password',, val => val === resetForm.new_password || `Password dosen't match`]" />
+
+                  <q-btn no-caps @click="submitOTP" type="submit" color="primary" label="Submit" class="q-mt-lg" />
+               </form>
+
+               <p class="q-mt-lg">
+                  Already have an account?
+                  <span @click="mode = 'signin'" class="text-primary cursor-pointer">Please Signin</span>
+                  or
+                  <span @click="mode = 'signup'" class="text-primary cursor-pointer">Create new</span>
+               </p>
+            </q-card-section>
+         </q-card>
+
       </q-dialog>
    </div>
 </template>
@@ -98,7 +154,9 @@ export default {
             email: '',
             password: ''
          },
+
          signupConfirmed: '',
+         resetConfirmed: '',
 
          forgetForm: {
             email: ''
@@ -119,6 +177,11 @@ export default {
       },
       async onSignin() {
          this.errorMessage = ''
+
+         if (!this.validateSignin()) {
+            return
+         }
+
          try {
             this.$q.loading.show({ message: 'Please wait...' })
             await this.signin(this.signinForm)
@@ -160,7 +223,7 @@ export default {
             this.$q.notify({
                message: 'Registration Successful!',
                color: 'positive',
-               position: 'top-right',
+               position: 'top-rightbottom',
                icon: 'done',
             })
 
@@ -171,7 +234,69 @@ export default {
             this.$q.notify({
                message: 'Registration Fail!',
                color: 'negative',
-               position: 'top-right',
+               position: 'bottom',
+               icon: 'cancel',
+            })
+         } finally {
+            this.$q.loading.hide()
+         }
+      },
+      async sendOtp() {
+         this.$refs.email.validate()
+         if (this.$refs.email.hasError) {
+            return
+         }
+
+         try {
+            this.$q.loading.show({ message: 'Please wait...' })
+
+            await this.$api.post('/send-otp', this.forgetForm)
+            this.mode = 'otp'
+
+            this.$q.notify({
+               message: 'Please check your mail',
+               color: 'positive',
+               position: 'bottom',
+               icon: 'done',
+            })
+
+         } catch (error) {
+            this.$q.notify({
+               message: 'Request Fail!',
+               color: 'negative',
+               position: 'bottom',
+               icon: 'cancel',
+            })
+         } finally {
+            this.$q.loading.hide()
+         }
+      },
+      async submitOTP() {
+         if (!this.validateResetForm()) {
+            return
+         }
+         try {
+            this.$q.loading.show({ message: 'Please wait...' })
+
+            await this.$api.post('/forgot-password', {
+               ...this.resetForm,
+               email: this.forgetForm.email
+            })
+
+            this.mode = 'signin'
+
+            this.$q.notify({
+               message: 'Request Success',
+               color: 'positive',
+               position: 'bottom',
+               icon: 'done',
+            })
+
+         } catch (error) {
+            this.$q.notify({
+               message: 'Request Fail!',
+               color: 'negative',
+               position: 'bottom',
                icon: 'cancel',
             })
          } finally {
@@ -191,6 +316,27 @@ export default {
             this.$refs.username.hasError ||
             this.$refs.phone.hasError ||
             this.$refs.email.hasError ||
+            this.$refs.password.hasError ||
+            this.$refs['confirmed-password'].hasError
+         )
+      },
+      validateSignin() {
+         this.$refs.user.validate()
+         this.$refs.password.validate()
+
+         return !(
+            this.$refs.user.hasError ||
+            this.$refs.password.hasError
+         )
+      },
+      validateResetForm() {
+         console.log('Validate')
+         this.$refs.otp.validate()
+         this.$refs.password.validate()
+         this.$refs['confirmed-password'].validate()
+
+         return !(
+            this.$refs.otp.hasError ||
             this.$refs.password.hasError ||
             this.$refs['confirmed-password'].hasError
          )
@@ -219,7 +365,8 @@ export default {
             otp: '',
             new_password: ''
          }
-      }
+      },
+
    },
 
    mounted() {
